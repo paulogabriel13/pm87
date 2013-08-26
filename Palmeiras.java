c140a14ab00b19b652096873b0e8e37d5d629356
Paulo <paulogabriel13@gmail.com>
2013-08-26 12:04:23 -0300

    Alterar classe Palmeiras.java

--- Palmeiras.java
+++ Palmeiras.java
@@ -1,7 +1,8 @@
 public class Palmeiras{
 
-public static void main (String args[]){
-
+public static void main (String[] args){
+	
+	system.out.println("Palmeiras Campeão Copa do Brasil 2013");
 	system.out.println("Palmeiras 99 anos!");
 
 }
